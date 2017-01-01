a85a1a75591f9794f51c7d4cfe874f77512e75d4
Sejin Han <sejin.han@ticketplace.net>
2017-01-01 17:49:36 +0900

    Add widget

--- node.js
+++ node.js
@@ -10,45 +10,86 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(function(req, res, next){
-   res.locals.showTests = app.get('env') !== 'production' &&
-       req.query.test === '1';
-   next();
+app.use(function (req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' &&
+        req.query.test === '1';
+    next();
 });
 
-app.get('/', function(req, res){
+app.use(function (req, res, next) {
+    if(!res.locals.partials) res.locals.partials = {};
+    res.locals.partials.weatherContext = getWeatherData();
+    next();
+});
+
+app.get('/', function (req, res) {
     res.render('home');
 });
 
-app.get('/about', function(req, res){
+app.get('/about', function (req, res) {
     res.render('about', {
-        fortune : fortune.getFortune(),
+        fortune: fortune.getFortune(),
         pageTestScript: '/qa/tests-about.js'
     });
 });
 
-app.get('/contact', function(req, res){
+app.get('/contact', function (req, res) {
     res.render('contact');
 });
 
-app.get('/tours/hood-river', function(req, res){
+app.get('/tours/hood-river', function (req, res) {
     res.render('tours/hood-river');
 });
 
-app.get('/tours/request-group-rate', function(req, res){
+app.get('/tours/request-group-rate', function (req, res) {
     res.render('tours/request-group-rate');
 });
 
-app.get('/tours/oregon-coast', function(req, res){
+app.get('/tours/oregon-coast', function (req, res) {
     res.render('tours/oregon-coast');
 });
 
-app.use(function(req, res, next){
+app.get('/headers', function (req, res) {
+    res.set('Content-Type', 'text/plain');
+    var s = '';
+    for (var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
+    res.send(s);
+});
+
+function getWeatherData() {
+    return {
+        locations: [
+            {
+                name: 'Portland',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Overcast',
+                temp: '54.1 F (12.3 C)',
+            },
+            {
+                name: 'Bend',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Partly Cloudy',
+                temp: '55.0 F (12.8 C)',
+            },
+            {
+                name: 'Manzanita',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Light Rain',
+                temp: '55.0 F (12.8 C)',
+            },
+        ]
+    }
+}
+
+app.use(function (req, res, next) {
     res.status(404);
     res.render('404');
 });
 
-app.use(function(err, req, res, next){
+app.use(function (err, req, res, next) {
     console.error(err.stack);
     res.status(500);
     res.render('500');
