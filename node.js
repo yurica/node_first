ef63ef3b04426d3a037d59fd508c946af18f5d3b
Sejin Han <sejin.han@ticketplace.net>
2017-01-02 09:08:17 +0900

    Add form

--- node.js
+++ node.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fortune = require('./lib/fortune.js');
 var app = express();
-
+var formidable = require('formidable');
 var handlebars = require('express-handlebars').create({
     defaultLayout: 'main',
     helpers: {
@@ -37,13 +37,21 @@ app.get('/newsletter', function (req, res) {
     res.render('newsletter', {csrf: 'CSRF token goes here'});
 });
 
-app.post('/process', function (req, res) {
-    console.log('Form (from querystring): ' + req.query.form);
-    console.log('CSRF token (from hidden form field): ' + req.body._csrf);
-    console.log('Name (from visible form field): ' + req.body.name);
-    console.log('Email (from visible form field): ' + req.body.email);
-    res.redirect(303, '/thank-you');
+app.post('/process', function(req, res){
+    if(req.xhr || req.accepts('json, html') === 'json'){
+        res.send({ success: true});
+    } else{
+        res.redirect(303, 'thank-you')
+    }
 });
+
+// app.post('/process', function (req, res) {
+//     console.log('Form (from querystring): ' + req.query.form);
+//     console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+//     console.log('Name (from visible form field): ' + req.body.name);
+//     console.log('Email (from visible form field): ' + req.body.email);
+//     res.redirect(303, '/thank-you');
+// });
 
 app.get('/', function (req, res) {
     res.render('home');
